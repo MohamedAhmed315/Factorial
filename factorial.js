8851a9fc58250095e410377134771959b5b24447
Mohamed Ahmed <mohamedahmed315@hotmail.com>
2021-01-16 00:45:08 +0200

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,15 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`Enter a number to compute factorial: `, (number) => {
+
+    let fact = 1;
+    for (let i = 1; i <= number; i++){
+        fact = fact * i;
+    }
+    console.log("Factorial " + number + " = " + fact);
+    readline.close();
+  });
+
